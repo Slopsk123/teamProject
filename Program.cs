d81450548187dd25c26c7e10bbcf46441765ce3a
Vítězslav Mrkos <vitamrkos@gmail.com>
2023-11-27 10:31:16 +0100

    27.11

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.ExceptionServices;
+{
+    string result = "";
+    string userInput = Console.ReadLine();
+
+    while (string.IsNullOrWhiteSpace(userInput) != true)
+    {
+        result += userInput;
+        userInput = Console.ReadLine();
+    }
+    Console.WriteLine(result);
+}
+
+{
+    string userInput2 = Console.ReadLine();
+    int i = 0;
+    while (string.IsNullOrWhiteSpace (userInput2) != true)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Console.WriteLine(userInput2);
+            i++;
+        }
+        
+    }
+}
